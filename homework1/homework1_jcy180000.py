badf5cc06585d209c2db1b0dde9edf7a74a0987e
jcy180000 <jcy180000@utdallas.edu>
2022-08-30 14:29:47 -0500

    Finalized homework 1

--- homework1/homework1_jcy180000.py
+++ homework1/homework1_jcy180000.py
@@ -5,32 +5,46 @@ import re
 
 
 def capitalize(name):
-	#capitalize first letter, lowercase all others
+	#Input: string name
+	#Output: name w/ capitalized first letter, lowercase all others
 	return name[0].upper()+name[1:].lower()
 
-def validate(x, pattern, msg):
+def validate(x, pattern, err, msg):
+	# Input: string x, regex pattern, string err, string msg
 	# keep asking for new input until x matches the given pattern
+	# Output: string x matching the pattern
 	while not re.match(pattern, x):
+		print(err.format(val=x))
 		x = input(msg)
 	return x
 
 class Person:
 	def __init__(self, last, first, mi, id_, phone):
+		#Input: string last, string first, string mi, string id_, string phone
 		#initialize object with proper attributes
+		#Output: self
 		self.last = capitalize(last)
 		self.first = capitalize(first)
 		self.mi = mi.upper() if mi else 'X'
-		self.id = validate(id_, r'[A-Z]{2}[0-9]{4}', 'Please enter a valid ID: ')
-		self.phone = validate(phone, r'[0-9]{3}-[0-9]{3}-[0-9]{4}', 'Please enter a valid phone number: ')
+		self.id = validate(id_, r'[A-Z]{2}[0-9]{4}', 
+			'ID is invalid: {val}\nID is two letters followed by 4 digits',
+			 'Please enter a valid ID: ')
+		self.phone = validate(phone, r'[0-9]{3}-[0-9]{3}-[0-9]{4}', 
+			'Phone {val} is invalid\nEnter phone number in form 123-456-7890', 
+			'Enter phone number: ')
 
 	def display(self):
-		#display formatted data
+		#print formatted data
+		#Output: null
 		print(f'Employee id: {self.id}')
 		print(f'\t{self.first} {self.mi} {self.last}')
 		print(f'\t{self.phone}')
 
 def processInput(fileName):
+	#Input: string fileName
 	#open file and read in each line to the object
+	#then return dict of people
+	#Output: dict of Person objects 
 	with open(os.path.join(os.getcwd(), fileName), mode='r', encoding='utf-8-sig') as inputFile:
 		inputFile.readline()
 		people = {}
@@ -39,17 +53,25 @@ def processInput(fileName):
 			attrs = line.strip().split(',')
 			p = Person(*attrs)		
 			if people.get(p.id):
-				p.id = input('Error: Multiple IDs. Re-enter a valid ID: ')	
+				# prompt for a different ID
+				p.id = input('Error: Multiple IDs. Enter a different ID: ')	
 			people[p.id] = p
 
+	return people
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		print('Insufficient arguments')
+
+	#process input based on given file name
 	people = processInput(sys.argv[1])
+	#dump dict to pickle file
 	pickle.dump(people, open('people.p', 'wb'))
+
+	#reopen pickle file and display output
 	print('Employee list:')
 	print()
-	for _,p in pickle.load(open('people.p', 'rb')).items():
-		p.display()
-		print()
+	with open('people.p', 'rb') as f:
+		for _,p in pickle.load(f).items():
+			p.display()
+			print()
